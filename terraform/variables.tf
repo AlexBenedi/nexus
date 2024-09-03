770217ea2ad1359d805bef5cf2bbec0e48d03543
AlexBenedi <843826@unizar.es>
2024-09-03 18:51:59 +0200

    Variables  credenciales añadidas

---
+++ terraform/variables.tf
@@ -0,0 +1,17 @@
+variable "nexus_url" {
+    description = "URL de Nexus"
+    type        = string
+    default     = ""
+}
+
+variable "nexus_username" {
+    description = "Usuario del admin de Nexus"
+    type        = string
+    default     = ""
+}
+
+variable "nexus_password" {
+    description = "Contraseña del admin de Nexus"
+    type        = string
+    default     = ""
+}
