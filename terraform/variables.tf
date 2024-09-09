0c0e3bc676f4570fdfe5274c15c04f3bb32a8b6b
AlexBenedi <843826@unizar.es>
2024-09-09 18:47:51 +0200

    Añadidas variables de usuarios, equipos y repositorio docker

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -15,3 +15,50 @@ variable "nexus_password" {
     type        = string
     default     = ""
 }
+
+variable "users" {
+    type = map(object({
+        email = string
+        firstname = string
+        lastname = string
+        password = optional(string, "")
+        userid = string
+        #roles = optional(set(string), [])
+        status = optional(string, "active")
+    }))
+}
+
+variable "teams" {
+    type = map(set(string))
+}
+
+/*
+variable "docker_repository" {
+    type = map(object({
+
+        name = string
+        online = optional(bool, true)
+
+        docker = object({
+          force_basic_auth = bool
+          v1_enabled = bool
+          http_port = optional(number)
+          http_ports = optional(number)
+          subdomain = optional(string)
+        })
+
+        storage = object({
+          blob_store_name = string
+          strict_content_type_validation = bool
+          write_policy = optional(string) 
+        })
+
+        cleanup = optional(object({
+            policy_names = optional(set(string), [])
+        }))
+
+        access = optional(set(string), [])
+    }))
+}
+*/
+
