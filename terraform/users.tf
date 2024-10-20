04cb7ed7f83ba20615a7435bfb24b1df8e5f9f48
AlexBenedi <843826@unizar.es>
2024-10-21 00:34:11 +0200

    Union usuarios y roles

--- terraform/users.tf
+++ terraform/users.tf
@@ -8,13 +8,30 @@ resource "nexus_security_user" "create" {
   lastname  = each.value.lastname
   email     = each.value.email
   password  = each.value.password
-  roles     = ["nx-admin"]
+  roles     = concat(lookup(local.roles_per_user, each.key, []), 
+    ["${each.value.userid}", "nx-base-role"])
   status    = each.value.status
 
   lifecycle {
-    ignore_changes = [ password ]
+    ignore_changes = [ firstname, lastname, email, password ]
   }
 
   depends_on = [ nexus_security_role.create_role ]
+}
+
+
+locals {
+  _roles_user = flatten([
+    for name, info in var.teams : [
+      for user in info.users : {
+        user = user
+        team = name
+      }
+    ]
+  ])
+
+  roles_per_user = {
+    for role in local._roles_user : role.user => role.team...
+  }
 }
 
