1a1811a019b75e1120f4262747714bc13a111b62
AlexBenedi <843826@unizar.es>
2024-10-20 18:18:56 +0200

    Creacion de roles con privilegios wildcards (no funciona)

---
+++ terraform/privileges.tf
@@ -0,0 +1,64 @@
+
+resource "nexus_privilege_wildcard" "read_privileges" {
+    for_each = {
+        for role, info in local.access_per_role : role => [
+            for permission in info : permission.repository
+            if permission.permission == "read"
+        ]
+    }
+
+    name = "${each.key}-read-privilege"
+    description = "Privilege read of ${each.key}"
+    pattern = "nexus:repository-view:*:${join(",", each.value)}:read,browse"
+
+    depends_on = [ local.access_per_role ]
+}
+
+resource "nexus_privilege_wildcard" "write_privileges" {
+    for_each = {
+        for role, info in local.access_per_role : role => [
+            for permission in info : permission.repository
+            if permission.permission == "write"
+        ]
+    }
+
+    name = "${each.key}-write-privilege"
+    description = "Privilege write of ${each.key}"
+    pattern = "nexus:repository-view:*:${join(",", each.value)}:edit,add,delete"
+
+    depends_on = [ local.access_per_role ]
+}
+
+output "debug" {
+  value = {
+        for role, info in local.access_per_role : role => [
+            for permission in info : permission.repository
+            if permission.permission == "write"
+        ]
+    }
+}
+
+locals {
+  docker_access = flatten([
+    for key, value in var.docker_repository : [
+      for role, permission in value.access : {
+        role = role
+        permission = permission
+        repository = key
+        format = "docker"
+      }
+    ]
+  ])
+
+  all_access = concat(local.docker_access)
+
+  access_per_role = {
+    for privilege in local.all_access : privilege.role => {
+      repository = privilege.repository
+      permission = privilege.permission
+      format = privilege.format
+    }...
+  }
+
+
+}
--- terraform/roles.tf
+++ terraform/roles.tf
@@ -4,13 +4,17 @@ resource "nexus_security_role" "create_role" {
   roleid = each.key
   name = each.key
   description = each.value.description
+
+  privileges = [ "${each.key}-read-privilege", "${each.key}-write-privilege" ]
+
+  depends_on =[nexus_privilege_wildcard.read_privileges, nexus_privilege_wildcard.write_privileges]
 }
 
-
 locals {
-    users_role = {for key, value in var.users : value.userid => {
-        roleid = value.userid
-        users = ["${value.userid}"]
-        description = "Pesonal team of user ${value.userid}"
-    }}
+  users_role = {for key, value in var.users : value.userid => {
+    roleid = value.userid
+    users = ["${value.userid}"]
+    description = "Pesonal team of user ${value.userid}"
+  }}
+
 }
